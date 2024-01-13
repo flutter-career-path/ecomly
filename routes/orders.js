462683dee2c8c9ec032dbeb4b03f5c6a5f6cfbfd
NonymousMorlock <agamspaul@gmail.com>
2024-01-13 02:17:16 +0000

    removed add order route since it is automatically triggered after checkout

--- routes/orders.js
+++ routes/orders.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orders');
 
-router.post('/', orderController.addOrder);
+// router.post('/', orderController.addOrder);
 
 router.get('/user/:userId', orderController.getUserOrders);
 
